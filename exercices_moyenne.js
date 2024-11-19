e10b9e6cbd4ae07df0ba2fa3dd2f40fdbaf546b1
BABIOUI-Fatima-Ezzahrae <fatimaezzahrae.babioui@gmail.com>
2024-11-19 21:53:51 +0100

    exercice 12!!

--- exercices_moyenne.js
+++ exercices_moyenne.js
@@ -4,3 +4,13 @@ let tab11 = [1,2,3,4,5];
 for (let i of tab11){
     console.log(i)
 }
+
+//exercice 12:
+let tab12 = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
+
+function EstPair(n) {
+    return n%2 === 0;
+}
+
+let resultat = tab12.filter(EstPair);
+console.log(resultat)
