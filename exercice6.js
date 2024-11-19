2de0f1f8ba0396e3f70fb02cf3651dead66d9d70
BABIOUI-Fatima-Ezzahrae <fatimaezzahrae.babioui@gmail.com>
2024-11-19 21:46:17 +0100

    exercice 11!!

---
+++ exercice6.js
@@ -0,0 +1,6 @@
+let obj1 = {
+    nom : "Fatima Ezzahrae",
+    age : 25
+}
+
+console.log(obj1);
---
+++ exercices_moyenne.js
@@ -0,0 +1,6 @@
+//exercice 11:
+let tab11 = [1,2,3,4,5];
+
+for (let i of tab11){
+    console.log(i)
+}
